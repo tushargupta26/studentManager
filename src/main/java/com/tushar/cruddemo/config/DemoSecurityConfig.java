56795a01d32a82ab9b1d09bdd0da3f599832e18f
tushargupta26 <tushargupta.tg2609@gmail.com>
2023-03-28 15:47:20 +0530

    Spring security JDBC authentication with passwords in DB

--- src/main/java/com/tushar/cruddemo/config/DemoSecurityConfig.java
+++ src/main/java/com/tushar/cruddemo/config/DemoSecurityConfig.java
@@ -4,36 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for jdbc... no more hard coded users :-)
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("STUDENT")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("STUDENT", "TEACHER")
-                .build();
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("STUDENT", "TEACHER", "ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
                         .requestMatchers(HttpMethod.GET, "/students").hasRole("STUDENT")
@@ -49,4 +37,24 @@ public class DemoSecurityConfig {
         http.csrf().disable();
         return http.build();
     }
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager(){
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("STUDENT")
+//                .build();
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("STUDENT", "TEACHER")
+//                .build();
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("STUDENT", "TEACHER", "ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
 }
